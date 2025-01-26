a85223db6e6f71bbd12aa5c2fbd5baa6017f5274
Omer C <639682+omercnet@users.noreply.github.com>
2025-01-26 12:40:00 +0200

    renovate lookup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function Lookup() {
+  const [config, setConfig] = useState({});
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    if (!config.manager)
+      setConfig({
+        ...config,
+        manager: "npm",
+        versioning: "npm",
+        rangeStrategy: "replace",
+        currentValue: "3.7.0",
+        packageName: "webpack",
+        datasource: "npm",
+      });
+  }, [config]);
+
+  const lookup = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(config),
+      });
+      setState(await response.json());
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <label>
+        packageName
+        <input
+          type="text"
+          value={config.packageName}
+          onChange={(e) =>
+            setConfig({ ...config, packageName: e.target.value })
+          }
+        />
+      </label>
+      <label>
+        currentValue
+        <input
+          type="text"
+          value={config.currentValue}
+          onChange={(e) =>
+            setConfig({ ...config, currentValue: e.target.value })
+          }
+        />
+      </label>
+      <label>
+        manager
+        <input
+          type="text"
+          value={config.manager}
+          onChange={(e) => setConfig({ ...config, manager: e.target.value })}
+        />
+      </label>
+      <label>
+        versioning
+        <input
+          type="text"
+          value={config.versioning}
+          onChange={(e) => setConfig({ ...config, versioning: e.target.value })}
+        />
+      </label>
+      <label>
+        rangeStrategy
+        <input
+          type="text"
+          value={config.rangeStrategy}
+          onChange={(e) =>
+            setConfig({
+              ...config,
+              rangeStrategy: e.target.value,
+            })
+          }
+        />
+      </label>
+      <label>
+        datasource
+        <input
+          type="text"
+          value={config.datasource}
+          onChange={(e) => setConfig({ ...config, datasource: e.target.value })}
+        />
+      </label>
+      <button type="submit" onClick={lookup}>
+        Renovate Lookup
+      </button>
+      <pre>
+        <code>{state && JSON.stringify(state, null, 2)}</code>
+      </pre>
+    </form>
   );
 }
-
-export default App;
